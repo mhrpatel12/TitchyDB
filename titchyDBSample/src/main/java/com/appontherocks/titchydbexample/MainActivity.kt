ef1bfcc675f1b508f0d6f2c6997be8c65a4b951e
Mihir <mhrpatel12@gmail.com>
2018-05-01 16:32:27 +0530

    Initial commit

---
+++ titchyDBSample/src/main/java/com/appontherocks/titchydbexample/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.appontherocks.titchydbexample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.appontherocks.titchydb.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
