8b2ad9416cd3bd689718e129daa54fbf4377b40b
Mihir <mhrpatel12@gmail.com>
2018-05-07 02:11:25 +0530

    new activity to test titchy db

--- titchyDBSample/src/main/java/com/appontherocks/titchydbexample/HomeActivity.java
+++ titchyDBSample/src/main/java/com/appontherocks/titchydbexample/HomeActivity.java
@@ -6,10 +6,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 import com.appontherocks.titchydb.R;
+import com.appontherocks.titchydb.TinyDB;
+import com.appontherocks.titchydb.TitchyDB;
 
 public class HomeActivity extends AppCompatActivity {
+
+    TitchyDB titchyDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +22,21 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        titchyDB = new TitchyDB(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                titchyDB.putString("new", "new");
+            }
+        });
+
+        FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.fab2);
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(HomeActivity.this, titchyDB.getString("new"), Toast.LENGTH_LONG).show();
             }
         });
     }
